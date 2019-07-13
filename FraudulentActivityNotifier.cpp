2fb088c525fd934027e4f44fdb4dc1d00b868667
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2019-07-13 09:00:25 +0530

    [WIP] Fraudulent Activity Notifier

---
+++ FraudulentActivityNotifier.cpp
@@ -0,0 +1,7 @@
+#include "FraudulentActivityNotifier.h"
+
+int FraudulentActivityNotifier::activityNotifications(vector<int> expenditure, int d)
+{
+
+	return 0;
+}
---
+++ FraudulentActivityNotifier.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class FraudulentActivityNotifier
+{
+public:
+	int activityNotifications(vector<int> expenditure, int d);
+};
+
