9fa2e7441b0ab8131caf31ce7ee63948c1ee7811
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2019-07-29 22:32:00 +0530

    wip

--- GridlandMetro.cpp
+++ GridlandMetro.cpp
@@ -1,5 +1,6 @@
 #include "GridlandMetro.h"
 
-
-
-
+int GridlandMetro::gridlandMetro(int n, int m, int k, vector<vector<int>> track)
+{
+	return 0;
+}
--- GridlandMetro.h
+++ GridlandMetro.h
@@ -1,5 +1,11 @@
 #pragma once
+
+#include <vector>
+
+using namespace std;
+
 class GridlandMetro
 {
+public:
+	int gridlandMetro(int n, int m, int k, vector<vector<int>> track);
 };
-
