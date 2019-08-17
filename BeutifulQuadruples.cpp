6535914e7ae7c7a7e336345ebd627614f43dd91a
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2019-08-17 20:02:25 +0530

    wip

--- BeutifulQuadruples.cpp
+++ BeutifulQuadruples.cpp
@@ -2,5 +2,21 @@
 
 int BeutifulQuadruples::beautifulQuadruples(int a, int b, int c, int d)
 {
+	vector<int> bq{ a, b, c, d };
+	sort(bq.begin(), bq.end());
+
+	int cnt = 0;
+
+
+
+	for (size_t i = 1; i <= bq[0]; i++)
+	{
+		for(size_t j = i; j <= bq[1]; j++)
+		{
+
+			cnt++;
+		}
+	}
+	
 	return 0;
 }
--- hr_cpp.cpp
+++ hr_cpp.cpp
@@ -28,3 +28,90 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+/*
+ * hakcerearth code arena
+ * raise them bones
+ /*
+
+// Write your code here
+#include<iostream>
+#include<vector>
+#include<map>
+#include<queue>
+
+using namespace std;
+
+int swampThing(int n, int r, int e, map<int, vector<vector<int>>> rds)
+{
+	map<int, int> visited;
+	map<int, int> dist;
+	map<int, int> energy;
+	queue<vector<int>> q;
+
+	int en = e;
+	int dd = 0;
+
+	q.push(vector<int>{1, 0, e});
+
+	visited[1] = 1;
+
+	while (true)
+	{
+		vector<int> loc = q.front();
+		q.pop();
+		for (auto i : rds[loc[0]])
+		{
+			q.push(vector<int>{i[0], loc[1] + i[1], loc[2] - i[2]});
+
+			if (en > loc[2] - i[2] && visited.find(i[0]) != visited.end() && visited.find(i[0]) == n) {
+				en = loc[2] - i[2];
+				dd = loc[1] + i[1];
+			}
+
+			if (visited.find(i[0]) == visited.end())
+			{
+				visited[i[0]] = 1;
+			}
+			else
+			{
+				visited[i[0]] += 1;
+			}
+		}
+
+		if (q.empty()) break;
+	}
+
+	return dd;
+}
+
+int main()
+{
+	int n, r, e;
+	cin >> n >> r >> e;
+
+	map<int, vector<vector<int>>> rds;
+	for (int i = 0; i < r; i++)
+	{
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+
+		if (rds.find(a) == rds.end())
+		{
+			rds[a] = vector<vector<int>>{ vector<int>{b, c, d} };
+		}
+		else
+		{
+			auto it = rds.find(a);
+			it->second.push_back(vector<int>{b, c, d});
+		}
+	}
+
+	int ans = swampThing(n, r, e, rds);
+
+	cout << ans << endl;
+
+	return 0;
+}
+
+*/
