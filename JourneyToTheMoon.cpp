4a84a4890b8559b2e4e41affd0aa9aa0cc80117f
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2019-09-04 21:53:49 +0530

    wip

--- JourneyToTheMoon.cpp
+++ JourneyToTheMoon.cpp
@@ -2,5 +2,20 @@
 
 int JourneyToTheMoon::journeyToMoon(int n, vector<vector<int>> astronaut)
 {
+	vector<int> links(n, -1);
+
+	for (vector<int> a_pair: astronaut)
+	{
+		links[a_pair[0]] = links[a_pair[0]] == -1 ? a_pair[0] : links[a_pair[0]];
+		links[a_pair[1]] = links[a_pair[0]] == -1 ? a_pair[1] : links[a_pair[1]];
+
+		int temp = links[a_pair[1]];
+		links[a_pair[1]] = links[a_pair[0]];
+
+		//int a_id = 1;
+
+
+	}
+
 	return 0;
 }
